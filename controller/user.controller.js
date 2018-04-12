9f977747e6bc875d069d0876ff1087ddc4148c83
LizbethFP <lizbeth.fp@gmail.com>
2018-04-11 19:30:37 -0500

    añadiendo data de usuario

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,29 +2,36 @@
 
 const User = require('../models/user');
 
-function prueba (request, response) {
+function prueba(request, response) {
     response.status(200).send({
         message: "Probando petición get"
     });
 }
 
-function saveUser (request, response) {
+function saveUser(request, response) {
     var user = new User();
 
     user.name = request.body.name;
     user.password = request.body.password;
+    user.email = request.body.email;
+    user.profile = request.body.profile;
+    user.jobTitle = request.body.jobTitle;
+    user.description = request.body.description;
+    user.projectId = request.body.projectId;
 
-    if (user.name && user.password) {
-        user.save( (error, userStored) => {
-            if (error) {
-                response.status(400).send({message: "Error al guardar"});
-            } else {
-                response.status(200).send({message: "Se guardo con exito"});
-            }
-        });
-    } else {
-        response.status(400).send({message: "Debe llenar los campos"});
-    }
+
+    user.save((error, userStored) => {
+        if (error) {
+            response.status(400).send({
+                message: "Error al guardar"
+            });
+        } else {
+            response.status(200).send({
+                message: "Se guardo con exito"
+            });
+        }
+    });
+
 }
 
 module.exports = {
