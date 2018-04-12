6b339f029bae75e735af396f1da3d6a149dcd39a
AraceliCumen <aycuevam@gmail.com>
2018-04-11 20:58:09 -0500

    preparing to heroku

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,38 +3,46 @@
 const User = require('../models/user');
 
 function prueba(request, response) {
-    response.status(200).send({
-        message: "Probando petición get"
-    });
+  response.status(200).send({
+    message: "Probando petición get"
+  });
 }
 
 function saveUser(request, response) {
-    var user = new User();
+  var user = new User();
 
-    user.name = request.body.name;
-    user.password = request.body.password;
-    user.email = request.body.email;
-    user.profile = request.body.profile;
-    user.jobTitle = request.body.jobTitle;
-    user.description = request.body.description;
-    user.projectId = request.body.projectId;
+  user.name = request.body.name;
+  user.password = request.body.password;
+  user.email = request.body.email;
+  user.profile = request.body.profile;
+  user.jobTitle = request.body.jobTitle;
+  user.description = request.body.description;
+  user.projectId = request.body.projectId;
 
 
-    user.save((error, userStored) => {
-        if (error) {
-            response.status(400).send({
-                message: "Error al guardar"
-            });
-        } else {
-            response.status(200).send({
-                message: "Se guardo con exito"
-            });
-        }
-    });
+  user.save((error, userStored) => {
+    if (error) {
+      response.status(400).send({
+        message: "Error al guardar"
+      });
+    } else {
+      response.status(200).send({
+        message: "Se guardo con exito"
+      });
+    }
+  });
+}
+
+function signIn(request, response){
+  // Instanciar el modelo para hacer peticiones
+  var user = new User();
+
+  user.password = request.body.password;
+  user.email = request.body.email;
 
 }
 
 module.exports = {
-    prueba,
-    saveUser
+  prueba,
+  saveUser
 };
