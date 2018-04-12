e13f0a7fc1eddbc7927f115810f410ff860ffbd4
AraceliCumen <aycuevam@gmail.com>
2018-04-12 05:03:36 -0500

    actualizando

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,52 +9,56 @@ function prueba(request, response) {
 }
 
 function saveUser(request, response) {
-    var user = new User();
+  var user = new User();
 
-    user.name = request.body.name;
-    user.password = request.body.password;
-    user.email = request.body.email;
-    user.profile = request.body.profile;
-    user.jobTitle = request.body.jobTitle;
-    user.description = request.body.description;
-    user.projectId = request.body.projectId;
+  user.name = request.body.name;
+  user.password = request.body.password;
+  user.email = request.body.email;
+  user.profile = request.body.profile;
+  user.jobTitle = request.body.jobTitle;
+  user.description = request.body.description;
+  user.projectId = request.body.projectId;
 
 
-    user.save((error, userStored) => {
-        if (error) {
-            response.status(400).send({
-                message: "Error al guardar"
-            });
-        } else {
-            response.status(200).send({
-                message: "Se guardo con exito"
-            });
-        }
-    });
+  user.save((error, userStored) => {
+    if (error) {
+      response.status(400).send({
+        message: "Error al guardar"
+      });
+    } else {
+      response.status(200).send({
+        message: "Se guardo con exito"
+      });
+    }
+  });
 }
 
 function signIn(request, response) {
-    var user = new User();
+  var user = new User();
 
-    user.password = request.body.password;
-    user.email = request.body.email;
+  user.password = request.body.password;
+  user.email = request.body.email;
 
-    User.findOne({'password': user.password, 'email': user.email}).exec( (error,userResult) => {
-        if (error) {
-            response.status(400).send({
-                message: "Error al guardar"
-            });
-        } else {
-            response.status(200).send({
-                usuario: userResult
-            });
-        }
-    });
-    
+  User.findOne({ 'password': user.password, 'email': user.email }).exec((error, userResult) => {
+    if (error) {
+      response.status(400).send({
+        message: "Error al guardar"
+      });
+    } else {
+      response.status(200).send({
+        usuario: userResult
+      });
+    }
+  });
+
 }
 
+// function.showUser({ 
+
+// });
+
 module.exports = {
-    prueba,
-    saveUser,
-    signIn
+  prueba,
+  saveUser,
+  signIn
 };
